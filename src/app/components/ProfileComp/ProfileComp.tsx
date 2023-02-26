9335f5186a421785bfeded8364750fb571bbbe1b
rboudrouss <r.boudrouss@gmail.com>
2023-02-26 18:37:30 +0100

    profile component

--- src/app/components/ProfileComp/ProfileComp.tsx
+++ src/app/components/ProfileComp/ProfileComp.tsx
@@ -1,1 +1,15 @@
-export {};
+import { APIUser } from "@/helper/APIwrapper";
+import styles from "./ProfileComp.module.css";
+
+export default function ProfileComp(props: { user: APIUser | null }) {
+  if (!props.user)
+  return <></>;
+
+  return (
+    <div>
+      {/* <img src={props.user.avatarImg} className={styles.avatar}/> */}
+      <h1>{props.user.username}</h1>
+      <p>{props.user.bio}</p>
+    </div>
+  );
+}

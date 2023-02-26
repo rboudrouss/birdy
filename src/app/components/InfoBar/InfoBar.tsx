e367c2249ce8efc1fc0dc27de982f8d4cc765900
rboudrouss <r.boudrouss@gmail.com>
2023-02-26 18:35:03 +0100

    navbar edit

--- src/app/components/InfoBar/InfoBar.tsx
+++ src/app/components/InfoBar/InfoBar.tsx
@@ -1,9 +1,24 @@
 import styles from "./InfoBar.module.css";
+import { FaGithub } from "react-icons/fa";
 
 export default function InfoBar() {
   return (
-    <>
-      <h1>Info Bar</h1>
-    </>
+    <div className={styles.infoBar}>
+      <div className={styles.footer}>
+        <div className={styles.icons}>
+          <a href="https://github.com/rboudrouss/birdy">
+            <FaGithub /> Github
+          </a>
+        </div>
+        <div className={styles.author}>
+          <span>
+            <a href="https://rboud.com">
+              rboud
+            </a>
+            - 2023 - MIT License
+          </span>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -15,38 +15,48 @@ export default function NavBar() {
 
   return (
     <header className={styles.header}>
-      <div className={styles.logo}></div>
+      {/* <div className={styles.logo}></div> */}
       <div className={styles.navigation}>
         <a href="/" className={styles.link}>
-          {/* <FontAwesomeIcon icon={faMap} className={styles.icon} /> */}
           <div className="textWrapper">
             <p>Home</p>
           </div>
         </a>
-        <a href="/login" className={styles.link}>
-          {/* <FontAwesomeIcon icon={faMap} className={styles.icon} /> */}
-          <div className="textWrapper">
-            <p>Login</p>
-          </div>
-        </a>
-        <a href="/register" className={styles.link}>
-          {/* <FontAwesomeIcon icon={faMap} className={styles.icon} /> */}
-          <div className="textWrapper">
-            <p>Register</p>
-          </div>
-        </a>
-        <a href="/profile" className={styles.link}>
-          {/* <FontAwesomeIcon icon={faMap} className={styles.icon} /> */}
-          <div className="textWrapper">
-            <p>Profile</p>
-          </div>
-        </a>
-        <a href="/logout" className={styles.link}>
-          {/* <FontAwesomeIcon icon={faMap} className={styles.icon} /> */}
-          <div className="textWrapper">
-            <p>Logout</p>
-          </div>
-        </a>
+        {!loggedIn && (
+          <a href="/login" className={styles.link}>
+            <div className="textWrapper">
+              <p>Login</p>
+            </div>
+          </a>
+        )}
+        {!loggedIn && (
+          <a href="/register" className={styles.link}>
+            <div className="textWrapper">
+              <p>Register</p>
+            </div>
+          </a>
+        )}
+        {loggedIn && (
+          <a href={`/profile/${userService.userId}`} className={styles.link}>
+            <div className="textWrapper">
+              <p>Profile</p>
+            </div>
+          </a>
+        )}
+        {loggedIn && (
+          <a href="/settings" className={styles.link}>
+            <div className="textWrapper">
+              <p>Settings</p>
+            </div>
+          </a>
+        )}
+        {loggedIn && (
+          <a href="/logout" className={styles.link}>
+            <div className="textWrapper">
+              <p>Logout</p>
+            </div>
+          </a>
+        )}
         <p>
           {loggedIn
             ? `Connected as ${userService.getConnectedUser()?.username}`

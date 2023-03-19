a4ba5818545570960c8890e49be540fd9b904cdc
rboudrouss <r.boudrouss@gmail.com>
2023-03-19 01:58:53 +0100

    added likes popup

---
+++ src/app/components/PopUp/LikesPopUp.tsx
@@ -0,0 +1,21 @@
+import { APIUser } from "@/helper/APIwrapper";
+import AvatarImg from "../AvatarImg/AvatarImg";
+import PopUp from "./PopUp";
+import styles from "./LikesPopUp.module.css"
+
+export default function LikesPopUp(props: {
+  Users: APIUser[];
+  onClose: (e: React.MouseEvent<HTMLElement>) => void | Promise<void>;
+  className?: string;
+}) {
+  return <PopUp onClose={props.onClose} className={`${props.className ?? ''} ${styles.popUp}`}>
+    <div className="">
+      {props.Users.map((user, i) => (
+        <div key={i} className={styles.userElement}>
+          <AvatarImg url={user.avatarImg} width={30} height={30} />
+          <span className={styles.usernameText}>{user.username}</span>
+        </div>
+      ))}
+    </div>
+  </PopUp>;
+}
--- src/app/components/PostComp/PostComp.tsx
+++ src/app/components/PostComp/PostComp.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { APIPost } from "@/helper/APIwrapper";
+import { APIPost, APIUser } from "@/helper/APIwrapper";
 import styles from "./PostComp.module.css";
 import { useEffect, useState } from "react";
 import { FaHeart, FaComment, FaLink, FaTrash } from "react-icons/fa";
@@ -9,6 +9,7 @@ import cookieWrapper from "@/helper/cookiewrapper";
 import PostForm from "../PostForm/PostForm";
 import { UserTag } from "../UserTag/UserTag";
 import SurePopUp from "../PopUp/SurePopUp";
+import LikesPopUp from "../PopUp/LikesPopUp";
 
 export default function PostComp(props: {
   data: APIPost;
@@ -19,17 +20,15 @@ export default function PostComp(props: {
   let post = props.data;
   const [liked, setLiked] = useState(false);
   const [showReply, setShowReply] = useState(props.showReply || false);
-  const [showPopUp, setShowPopUp] = useState(false);
-
+  const [showDeletePopUp, setShowDeletePopUp] = useState(false);
+  const [showLikesPopUp, setShowLikesPopUp] = useState(false);
 
   useEffect(() => {
     setLiked(userService.isPostLiked(props.data.id));
   }, [props.data.id]);
 
   const likeHandler = (e: any) => {
-    e.preventDefault();
-    // HACK might be redundant but let's try
-    userService.updateConnectedUser();
+    e.stopPropagation();
     if (!cookieWrapper.front.isConnected()) return;
 
     if (liked) {
@@ -41,76 +40,101 @@ export default function PostComp(props: {
     }
     setLiked(!liked);
     userService.updateConnectedUser();
+    if(liked){
+      post.likesUsers = post.likesUsers.filter(user => user.id !== userService.userId);
+    } else {
+      post.likesUsers.push(userService.getConnectedUser() as APIUser);
+    }
   };
 
   const replyHandler = (e: any) => {
-    e.preventDefault();
+    e.stopPropagation();
     setShowReply(!showReply);
   };
 
   const deleteHandler = async (e: any) => {
-    e.preventDefault();
-    setShowPopUp(false);
+    e.stopPropagation();
+    setShowDeletePopUp(false);
     await post.delete();
     window.location.href = "/";
   };
 
   const clickDelete = (e: any) => {
-    e.preventDefault();
-    setShowPopUp(true);
+    e.stopPropagation();
+    setShowDeletePopUp(true);
   };
 
   return (
-    <div className={`${props.className} ${styles.all}`}>
-      {showPopUp && (
-        <SurePopUp onYes={deleteHandler} onClose={() => setShowPopUp(false)} />
+    <div className={`${props.className ?? ""} ${styles.all}`}>
+      {showDeletePopUp && (
+        <SurePopUp
+          onYes={deleteHandler}
+          onClose={() => setShowDeletePopUp(false)}
+        />
       )}
-      <a href={props.data.postLink}>
-        <div className={styles.julesDiv}>
-          {props.data.replyId && (
-            <div className={styles.responseSpace}>
-              <div className={styles.responseBar}></div>
-            </div>
-          )}
+      {showLikesPopUp && (
+        <LikesPopUp
+          onClose={() => setShowLikesPopUp(false)}
+          Users={props.data.likesUsers}
+        />
+      )}
+      <div className={styles.julesDiv}>
+        {props.data.replyId && (
+          <div className={styles.responseSpace}>
+            <div className={styles.responseBar}></div>
+          </div>
+        )}
 
-          <article className={styles.wrapper}>
-            <div className={styles.userDiv}>
-              <UserTag post={props.data} className={styles.userTag} />
-              {userService.userId == props.data.author?.id ? (
-                <>
-                  <button onClick={clickDelete} className={styles.delete}>
-                    {/* Delete */}
-                    <FaTrash />
-                  </button>
-                </>
-              ) : (
-                <div />
-              )}
-            </div>
-            <div className={styles.contentDiv}>
-              <span className={styles.content}>
-                {props.data.content.trim()}
-              </span>
-            </div>
-            <div className={styles.buttonDiv}>
-              <button onClick={replyHandler}>
-                {/* Comment */}
-                <p>{props.data.nbReplies}</p>
-                <FaComment />
-              </button>
+        <article
+          className={styles.wrapper}
+          onClick={() => {
+            window.location.href = props.data.postLink;
+          }}
+        >
+          <div className={styles.userDiv}>
+            <UserTag post={props.data} className={styles.userTag} />
+            {userService.userId == props.data.author?.id ? (
+              <>
+                <button onClick={clickDelete} className={styles.delete}>
+                  {/* Delete */}
+                  <FaTrash />
+                </button>
+              </>
+            ) : (
+              <div />
+            )}
+          </div>
+          <div className={styles.contentDiv}>
+            <span className={styles.content}>{props.data.content.trim()}</span>
+          </div>
+          <div className={styles.buttonDiv}>
+            <button onClick={replyHandler}>
+              {/* Comment */}
+              <p>{props.data.nbReplies}</p>
+              <FaComment />
+            </button>
+            <div className={styles.likesDiv}>
               <button onClick={likeHandler}>
                 {/* Like */}
-                <p>{props.data.nbLikes}</p>
                 <FaHeart color={liked ? "red" : "black"} />
               </button>
-              <button>
-                {/* Share Link */}
-                <FaLink />
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setShowLikesPopUp(!showLikesPopUp);
+                }}
+                className={styles.showLikes}
+              >
+                <p className={styles.nbLikesText}>{props.data.nbLikes}</p>
               </button>
             </div>
-          </article>
-        </div>
-      </a>
+            <button>
+              {/* Share Link */}
+              <FaLink />
+            </button>
+          </div>
+        </article>
+      </div>
       {showReply && (
         <PostForm
           parentPost={props.data}
